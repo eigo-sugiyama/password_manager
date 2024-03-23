1024d79f436c67f71a2642fd8f5f3a4084716a06
eigo-sugiyama <eigo.sugiyama0403@gmail.com>
2024-03-23 21:54:11 +0900

    step1

---
+++ password_manager.sh
@@ -0,0 +1,11 @@
+echo パスワードマネージャーへようこそ！
+echo サービス名を入力してください：
+read service_name
+echo ユーザー名を入力してください：
+read user_name
+echo パスワードを入力してください：
+read password
+echo $service_name
+echo $user_name
+echo $password
+echo 'Thank you!'
