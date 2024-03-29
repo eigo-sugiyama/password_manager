d05d1a1200a29f544267fc00dabeebbcb9582b22
eigo-sugiyama <eigo.sugiyama0403@gmail.com>
2024-03-29 10:34:09 +0000

    step3

--- password_manager.sh
+++ password_manager.sh
@@ -1,4 +1,4 @@
-#step2
+#step3
 echo パスワードマネージャーへようこそ！
 echo "次の選択肢から入力してください(Add Password/Get Password/Exit)："
 select input in "Add Password" "Get Password" "Exit"
@@ -12,15 +12,14 @@ case "$input" in
         #パスワードを表示させたくなければ、-pオプション
         #read -s -p "パスワードを入力してください： " set_password
         read -p "パスワードを入力してください： " set_password
-        echo $set_user_name > $set_service_name.txt
-        echo $set_password >> $set_service_name.txt
+        echo -e "ユーザー名: $set_user_name\nパスワード: $set_password" | gpg --encrypt --recipient 'eigo.sugiyama0403@gmail.com' --output "${set_service_name}.gpg"
         echo パスワードの追加は成功しました。
         ;;
     "Get Password")
         read -p "サービス名を入力してください： " get_service_name
-        if [[ -f "$get_service_name.txt" ]]; then
+        if [[ -f "$get_service_name.gpg" ]]; then
                 echo "ユーザー名とパスワードは以下の通りです："
-                cat "$get_service_name.txt"
+                gpg --decrypt "$get_service_name.gpg"
         else
             echo "指定されたサービスの情報は見つかりませんでした。"
         fi
@@ -33,7 +32,3 @@ case "$input" in
     ;;
 esac
 done
-
-
-
-
